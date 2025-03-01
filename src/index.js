87be1b7c84978e360666f947b81eff4da25d4b5f
Sebas807 <sebax807@gmail.com>
2025-02-28 22:02:26 -0500

    Configuración de firebase y servidor

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const teamsRoutes = require("./routes/teams");
+const leaguesRoutes = require("./routes/leagues");
+const playersRoutes = require("./routes/players");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/leagues", teamsRoutes);
+app.use("/api/leagues", leaguesRoutes);
+app.use("/api/leagues", playersRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
