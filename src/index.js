835ba1ce15646d0839b55813ff89745f006a634f
Sebas807 <sebax807@gmail.com>
2025-03-12 14:44:59 -0500

    Probando cambio en el Branch Tracking

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use("/api/leagues", leaguesRoutes);
 app.use("/api/leagues", playersRoutes);
 
 app.get("/", (req, res) => {
-  res.send("¡Bienvenido a la API de fútbol!");
+  res.send("¡Bienvenido a la API Rest de fútbol!");
 });
 
 if (process.env.NODE_ENV !== "test") {
