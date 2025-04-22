7f049f55dbf1d04970046103c3082d565a8cd78a
Sebas807 <sebax807@gmail.com>
2025-04-22 17:35:23 -0500

    Corrección de error de sintaxis en worker

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 const { ServiceBusClient } = require("@azure/service-bus");
 const axios = require("axios");
-const db = require("../firebase-admin");
+const db = require("./firebase-admin");
 
 dotenv.config();
 
